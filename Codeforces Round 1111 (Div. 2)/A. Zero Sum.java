2f50a3f9a087092e71db0ba369c8c53a658574a5
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-23 23:53:14 +0530

    Update A. Zero Sum.java

--- Codeforces Round 1111 (Div. 2)/A. Zero Sum.java
+++ Codeforces Round 1111 (Div. 2)/A. Zero Sum.java
@@ -1,1 +1,64 @@
+import java.io.*;
+import java.util.*;
 
+public class Main {
+
+    static class FastReader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    static FastReader sc = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void solve() throws Exception {
+        int n = sc.nextInt();
+
+        if ((n & 1) == 1) {
+            int i = 0;
+            while (i < n) {
+                sc.nextInt();
+                i++;
+            }
+            out.println("NO");
+            return;
+        }
+
+        int c = 0;
+        int i = 0;
+
+        while (i < n) {
+            if (sc.nextInt() == -1) {
+                c++;
+            }
+            i++;
+        }
+
+        if ((c & 1) == ((n / 2) & 1)) {
+            out.println("YES");
+        } else {
+            out.println("NO");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            solve();
+        }
+
+        out.flush();
+    }
+}
